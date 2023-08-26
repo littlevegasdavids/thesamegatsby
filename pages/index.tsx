63de4b213f2d07892ab2cae562b1541298ae4b0a
Reno Davids <reno.davids@sovtech.com>
2023-08-26 17:09:24 +0200

    Added Music and Software tab

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,11 @@ import {
   Icon,
   SimpleGrid,
   Spinner,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
   Text,
 } from "@chakra-ui/react";
 import {
@@ -20,13 +25,15 @@ import Image from "next/image";
 const Home: NextPage = () => {
   return (
     <Flex height="100vh" alignItems="center" justifyContent="center" m={[3, 0]}>
-      <Box p={2} rounded="lg" className="CardBorder" shadow="2xl" textColor="white" textAlign="center">
-        <Box
-          bg="black"
-          px={["3", "10"]}
-          py="3"
-          rounded="lg"
-        >
+      <Box
+        p={2}
+        rounded="lg"
+        className="CardBorder"
+        shadow="2xl"
+        textColor="white"
+        textAlign="center"
+      >
+        <Box bg="black" px={["3", "10"]} py="3" rounded="lg">
           <Flex
             flexDirection="column"
             gap={5}
@@ -39,50 +46,23 @@ const Home: NextPage = () => {
               width={400}
               height={60}
             />
-            <Text as="b" fontSize={["xl","3xl"]} mt="-10">THE SAME GATSBY</Text>
-            <Flex flexDirection="column" gap={0}>
-              <Text>Music | Alter Ego | Software</Text>
-              <Text>Cape Town, South Africa</Text>
-              <Text as="b">Under Construction: Coming Soon</Text>
-            </Flex>
-            <Flex gap={3}>
-              <a href="https://www.instagram.com/thesamegatsby" target="_blank">
-                <Icon
-                  as={InstagramLogo}
-                  boxSize={10}
-                  bg="white"
-                  textColor="black"
-                  rounded="lg"
-                />
-              </a>
-              <a href="https://www.youtube.com/@thesamegatsby" target="_blank">
-                <Icon
-                  as={YoutubeLogo}
-                  boxSize={10}
-                  bg="white"
-                  textColor="black"
-                  rounded="lg"
-                />
-              </a>
-              <a href="https://soundcloud.com/dejagatsby" target="_blank">
-                <Icon
-                  as={SoundcloudLogo}
-                  boxSize={10}
-                  bg="white"
-                  textColor="black"
-                  rounded="lg"
-                />
-              </a>
-              <a href="https://github.com/littlevegasdavids/littlevegasdavids" target="_blank">
-                <Icon
-                  as={GithubLogo}
-                  boxSize={10}
-                  bg="white"
-                  textColor="black"
-                  rounded="lg"
-                />
-              </a>
-            </Flex>
+            <Text as="b" fontSize={["xl", "3xl"]} mt="-10">
+              THE SAME GATSBY
+            </Text>
+            <Tabs isFitted variant="unstyled" w="100%">
+              <TabList>
+                <Tab _selected={{ color: "black", bg: "white" }}>Music</Tab>
+                <Tab _selected={{ color: "black", bg: "white" }}>Software</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <p>Music Tab</p>
+                </TabPanel>
+                <TabPanel>
+                  <p>Software Tab</p>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
           </Flex>
         </Box>
       </Box>
